aaa19affd159676d3f05706c1d61214fc0deed9f
DonateAngel <705025@student.dusd.net>
2023-12-16 01:24:57 -0800

    Didnt completly finish
    Having trouble with fixing error

--- WaypointsDonatePer4/Assets/Graphs/Graph.cs
+++ WaypointsDonatePer4/Assets/Graphs/Graph.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class Graph
 {
-    List<Edge> edges = new List<Edge>();
-    List<Node> nodes = new List<Node>();
-    List<Node> pathList = new List<Node>();
+    public List<Edge> edges = new List<Edge>();
+    public List<Node> nodes = new List<Node>();
+    public List<Node> pathList = new List<Node>();
 
     public Graph() {}
 
@@ -38,6 +38,83 @@ public class Graph
         }
         return null;
     }
+    public bool Astar(GameObject startId, GameObject endId)
+    {
+        Node start = FindNode(startId);
+        Node end = FindNode(endId);
+
+        if(start == null || end == null)
+        {
+            return false;
+        }
+
+        List<Node> open = new List<Node>();
+        List<Node> closed = new List<Node>();
+        float tentative_g_score = 0;
+        bool tentative_is_better;
+        start.g =0;
+        start.h = distance(start, end);
+        start.f = start.h;
+
+        open.Add(start);
+        while(open.Count>0)
+        {
+            int i = lowestF(open);
+            Node thisNode = open[i];
+            if (thisNode.getId() == endId)
+            {
+                ReconstructPath(start,end);
+                return true;
+            }
+            open.RemoveAt(i);
+            closed.Add(thisNode);
+            Node neighbour;
+            foreach (Edge e in thisNode.edgeList)
+            {
+                neighbour = e.endNode;
+
+                if (closed.IndexOf(neighbour) > -1)
+                continue;
+
+                tentative_g_score = thisNode.g +distance(thisNode, neighbour);
+                if(open.IndexOf(neighbour) ==-1 )
+                {
+                    open.Add(neighbour);
+                    tentative_is_better = true;
+                }
+                else if (tentative_g_score < neighbour.g)
+                {
+                    tentative_is_better = true;
+                }
+                else 
+                    tentative_is_better = false;
+
+                 if (tentative_is_better)
+                 {
+                     neighbour.cameFrom = thisNode;
+                     neighbour.g = tentative_g_score;
+                     neighbour.h = distance(thisNode,end);
+                     neighbour.f = neighbour.g + neighbour.h;
+                 }
+
+            }
+        }
+        return false;
+    }
+    public void ReconstructPath(Node startId,Node endId)
+    {
+        pathList.Clear();
+        pathList.Add(endId);
+
+        var p = endId.cameFrom;
+        while(p != startId && p != null)
+        {
+            pathList.Insert(0, p);
+            p=p.cameFrom;
+        }
+        pathList.Insert(0,startId);
+    }
+
     float distance(Node a, Node b)
     {
         return (Vector3.SqrMagnitude(a.getId().transform.position - b.getId().transform.position));
