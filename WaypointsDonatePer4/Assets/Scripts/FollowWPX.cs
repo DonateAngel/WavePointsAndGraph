7002f396a039775ac7d2e07ed8e151363c643e31
DonateAngel <705025@student.dusd.net>
2024-02-25 14:17:50 -0800

    Revert "Finished project"
    This reverts commit 37199dc2c4e4ae8221022e87b4a328258268c220.

--- WaypointsDonatePer4/Assets/Scripts/FollowWPX.cs
+++ WaypointsDonatePer4/Assets/Scripts/FollowWPX.cs
@@ -19,13 +19,13 @@ public class FollowWPX : MonoBehaviour
     {
         wps = wpManager.GetComponent<WPManager>().waypoints;
         g = wpManager.GetComponent<WPManager>().graph;
-        currentNode = wps[3];
+        currentNode = wps[0];
 
         Invoke("GoToRuin",2);
     }
     public void GoToHeli()
     {
-        g.Astar(currentNode, wps[4]);
+        g.Astar(currentNode, wps[0]);
         currentWP = 0;
     }
     public void GoToRuin()
@@ -37,7 +37,7 @@ public class FollowWPX : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        if (g.pathList.Count == 0 || currentWP == g.pathList.Count)
+        if(g.pathList.Count == 0 || currentWP == g.pathList.Count)
         return;
 
         if (Vector3.Distance(g.pathList[currentWP].getId().transform.position,
@@ -52,7 +52,8 @@ public class FollowWPX : MonoBehaviour
             Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y,goal.position.z);
             
             Vector3 direction = lookAtGoal - this.transform.position;
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                        Time.deltaTime * rotSpeed);
             this.transform.Translate(0,0, speed * Time.deltaTime);
         }
     }
